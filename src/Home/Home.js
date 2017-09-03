80ed558bdc5fc70f2957211fc9bdbe895dd985ba
Justin Hardin <jpmitchell293@gmail.com>
2017-09-03 13:34:40 -0700

    new projects

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,6 +3,11 @@ import './Home.css';
 
 import vac from '../images/vac.gif';
 import trinet from '../images/trinet-thumb.gif';
+import designmap from '../images/designmap-thumb.gif';
+import creatimator from '../images/creatimator.gif';
+import swiper from '../images/swiper.gif';
+import bankrate from '../images/bankrate-thumb.gif';
+import justin from '../images/justin-thumb.gif';
 
 class Home extends Component {
   render() {
@@ -12,8 +17,7 @@ class Home extends Component {
           <div className="nav-wrapper container">
             <div className="logo">
               <a href="">
-                <h2>Justin</h2>
-                <h2>Hardin</h2>
+                <h2>Justin Hardin</h2>
               </a>
             </div>
             <div className="hamburger">
@@ -33,32 +37,91 @@ class Home extends Component {
           <div className="row">
             <div className="col-md-4">
               <div className="project-thumb">
-                <img src={vac} alt=""/>
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>140 Proof</h3>
+                    </div>
+                    <img src={vac} alt=""/>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="col-md-4">
-              <div className="project-thumb"></div>
+              <div className="project-thumb">
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>Butchershop</h3>
+                    </div>
+                    <img src={creatimator} alt=""/>
+                  </div>
+                </a>
+              </div>
             </div>
             <div className="col-md-4">
-              <div className="project-thumb"></div>
+              <div className="project-thumb">
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>Designmap</h3>
+                    </div>
+                    <img src={designmap} alt=""/>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
           <div className="row">
             <div className="col-md-4">
-              <div className="project-thumb"></div>
+              <div className="project-thumb">
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>Bankrate</h3>
+                    </div>
+                    <img src={bankrate} alt=""/>
+                  </div>
+                </a>
+              </div>
             </div>
             <div className="col-md-8">
               <div className="project-thumb">
-                <img src={trinet} alt=""/>
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>TriNet</h3>
+                    </div>
+                    <img src={trinet} alt=""/>
+                  </div>
+                </a>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-md-4">
-              <div className="project-thumb"></div>
+              <div className="project-thumb">
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>Justinhardin.me</h3>
+                    </div>
+                    <img src={justin} alt=""/>
+                  </div>
+                </a>
+              </div>
             </div>
             <div className="col-md-8">
-              <div className="project-thumb"></div>
+              <div className="project-thumb">
+                <a href="">
+                  <div className="thumb-inner">
+                    <div className="thumb-hover">
+                       <h3>Swiper</h3>
+                    </div>
+                    <img src={swiper} alt=""/>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
